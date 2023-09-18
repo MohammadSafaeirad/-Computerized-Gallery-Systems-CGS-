62de5fc2d7eb2870ed46058e5a618fd5bf90dafb
mohammadsr1 <m.safaei81@hotmail.com>
2023-09-18 11:29:30 -0400

    Final commit for Computerized Gallery Systems(CGS)

---
+++ CGS_Database/Login.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace CGS_Database
+{
+    public partial class Login : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-D9754PO;Initial Catalog=CGS;Integrated Security=True");
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);    
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            try {
+                string query = "SELECT * FROM login WHERE Username='"+textBoxUsername.Text+"' AND Password='"+textBoxPassword.Text+"'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    this.Hide();
+                    Curator cur = new Curator();
+                    cur.Show();
+                    Artist artist = new Artist();
+                    artist.Show();
+                    ArtPiece piece = new ArtPiece();
+                    piece.Show();
+                    Sell sell = new Sell();
+                    sell.Show();
+                }
+                else {
+                    MessageBox.Show("Error! Credentials are incorrect!");
+                    textBoxUsername.Clear();
+                    textBoxPassword.Clear();
+                }
+            }
+            catch {
+                MessageBox.Show("An Exception happened!");
+            }
+            finally { 
+                conn.Close();
+            }
+        }
+    }
+}
